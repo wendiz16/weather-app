8fbcb16a0a6fe7269c2824f0d812d448b653d089
Wendi Zhang <wendizhang68@gmail.com>
2022-08-21 22:58:09 -0400

    change api call to https

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
   }
   // fetch weather 
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric`);
     const responseData = await response.json();
     return responseData;
   }
