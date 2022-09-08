5383b75025ed39810205d57b6173d8cbdebb17af
Wendi Zhang <wendizhang68@gmail.com>
2022-09-08 15:59:00 -0400

    updated ui.js and index.html

--- ui.js
+++ ui.js
@@ -5,7 +5,7 @@ class UI {
     this.string = document.getElementById('w-string');
     this.details = document.getElementById('w-details');
     this.icon = document.getElementById('w-icon');
-    this.mintemp = document.getElementById('w-temp-min')
+    this.feelsliketemp = document.getElementById('w-temp-feels-like')
     this.humidity = document.getElementById('w-humidity');
     this.windspeed = document.getElementById('w-windspeed');
     this.visibility = document.getElementById('w-visibility');
@@ -16,9 +16,9 @@ class UI {
     this.string.textContent = `${Math.round(weather.main.temp)} ℃`;
     this.icon.setAttribute('src',
       `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
-    this.mintemp.textContent = `Min Temperature : ${Math.round(weather.main.temp_min)} ℃`;
+    this.feelsliketemp.textContent = `Feels Like : ${Math.round(weather.main.feels_like)} ℃`;
     this.humidity.textContent = `Relative Humidity : ${weather.main.humidity}%`;
     this.windspeed.textContent = `Wind-Speed : ${weather.wind.speed}meter/sec`;
-    this.visibility.textContent = `Visibility : ${weather.visibility}`;
+    this.visibility.textContent = `Visibility : ${weather.visibility}m`;
   }
 }
